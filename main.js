e3f4a83c60844e5e09e7611daccd713dfad5446e
Bernabe <bernatattoo@gmail.com>
2022-03-29 23:21:52 -0300

    New Menu and JS

---
+++ main.js
@@ -0,0 +1,17 @@
+function menuToggle() {
+	let nav = document.getElementById('menu-overlay');
+	nav.classList.toggle('active');
+    {
+    let nav = document.getElementById('toggleIcon');
+	nav.classList.toggle('active');
+}
+}
+
+function reset() {
+    let nav = document.getElementById('menu-overlay');
+	nav.classList.remove('active');
+    {
+        let nav = document.getElementById('toggleIcon');
+	nav.classList.remove('active');
+    }
+}
